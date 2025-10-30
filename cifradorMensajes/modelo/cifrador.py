01cf7fdd9baba9e5d1efe9fda5f9f94cb41d99df
Samuel Romaña <wassamuel@gmail.com>
2025-10-30 17:19:41 -0500

    Se comienza a implementar y desarrolar las clases por tipo de cifrado

--- cifradorMensajes/modelo/cifrador.py
+++ cifradorMensajes/modelo/cifrador.py
@@ -23,6 +23,73 @@ class ReglaCifrado(ABC):
     def encontrar_no_ascii_mensaje(self, mensaje: str) -> list:
         pass
 
+class ReglaCifradoTraslacion(ReglaCifrado):
+    def __init__(self):
+        super().__init__()
+
+    def mensaje_valido(self, mensaje: str) -> bool:
+        if mensaje in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            return False
+
+        if mensaje == " " or mensaje in "@_#$o%":
+            return False
+
+        for letras in mensaje:
+            if letras not in string.ascii_lowercase:
+                return False
+
+        return True
+
+    def encriptar(self, mensaje: str) -> str:
+
+        mensaje_encriptado = ""
+
+        for caracter in mensaje:
+            x = mensaje.index(caracter)
+
+    def desencriptar(self, mensaje: str) -> str:
+        pass
+
+class ReglaCifradoNumerico(ReglaCifrado):
+    def __init__(self):
+        super().__init__()
+
+    def mensaje_valido(self, mensaje: str) -> bool:
+        if mensaje in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            return False
+
+        if mensaje[0] == " " or mensaje[len(mensaje)] == " ":
+            return False
+
+        contador_espacios = 0
+        hay_espacio = False
+        for caracteres in mensaje:
+            if caracteres == " ":
+                contador_espacios += 1
+                hay_espacio = True
+
+            #TODO: Continuar...
+
+
+    def encriptar(self, mensaje: str) -> str:
+        pass
+
+    def desencriptar(self, mensaje: str) -> str:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
